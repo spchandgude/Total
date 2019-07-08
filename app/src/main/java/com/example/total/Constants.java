500034b55f46975d45a50a594c1d0551391c2b00
spchandgude <shubhampchandgude@gmail.com>
2019-07-08 14:08:18 +0530

    #commit2 Registration i.e Main Activity created...

---
+++ app/src/main/java/com/example/total/Constants.java
@@ -0,0 +1,6 @@
+package com.example.total;
+
+public class Constants {
+    private static final String ROOT_URL="https://totallpgservice.000webhostapp.com/connect/Android2/v1/";
+    public  static final String URL_REGISTER=ROOT_URL+"registerUser.php";
+}
--- app/src/main/java/com/example/total/MainActivity.java
+++ app/src/main/java/com/example/total/MainActivity.java
@@ -2,13 +2,145 @@ package com.example.total;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText edit_lpg_id;
+
+    private EditText edit_first_name;
+    private EditText edit_last_name;
+    private EditText edit_contact_no;
+    private EditText edit_email_id;
+    private EditText edit_state;
+    private EditText edit_city;
+    private EditText edit_pincode;
+    private EditText edit_landmark;
+    private EditText edit_addressline1;
+    private EditText edit_addressline2;
+    private EditText edit_adhaar_no;
+    private EditText edit_gst_no;
+    private EditText edit_password;
+    private Button buttonRegister;
+    private ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edit_lpg_id = (EditText) findViewById(R.id.id_lpg_id);
+
+        edit_first_name = (EditText) findViewById(R.id.id_consumer_first_name);
+        edit_last_name = (EditText) findViewById(R.id.id_consumer_last_name);
+        edit_contact_no=(EditText) findViewById(R.id.id_consumer_contact_no);
+        edit_email_id=(EditText) findViewById(R.id.id_consumer_email_id);
+        edit_state=(EditText) findViewById(R.id.id_consumer_state);
+        edit_city=(EditText) findViewById(R.id.id_consumer_city);
+        edit_pincode=(EditText) findViewById(R.id.id_consumer_pincode);
+        edit_landmark=(EditText) findViewById(R.id.id_consumer_landmark);
+        edit_addressline1=(EditText) findViewById(R.id.id_consumer_addressline1);
+        edit_addressline2=(EditText) findViewById(R.id.id_consumer_addressline2);
+        edit_adhaar_no=(EditText) findViewById(R.id.id_consumer_adhaar_no);
+        edit_gst_no=(EditText)  findViewById(R.id.id_consumer_gst_no);
+        edit_password=(EditText) findViewById(R.id.id_password);
+
+        buttonRegister = (Button) findViewById(R.id.btn_signup);
+        progressDialog = new ProgressDialog(this);
+        buttonRegister.setOnClickListener(this);
+    }
+
+    private void registerUser(){
+        final String lpg_id = edit_lpg_id.getText().toString().trim();
+
+        final String first_name = edit_first_name.getText().toString().trim();
+        final String last_name = edit_last_name.getText().toString().trim();
+        final String contact_no = edit_contact_no.getText().toString().trim();
+        final String email_id = edit_email_id.getText().toString().trim();
+        final String state = edit_state.getText().toString().trim();
+        final String city = edit_city.getText().toString().trim();
+        final String pincode = edit_pincode.getText().toString().trim();
+        final String landmark = edit_landmark.getText().toString().trim();
+        final String addressline1 = edit_addressline1.getText().toString().trim();
+        final String addressline2 = edit_addressline2.getText().toString().trim();
+        final String adhaar_no = edit_adhaar_no.getText().toString().trim();
+        final String gst_no = edit_gst_no.getText().toString().trim();
+        final String password = edit_password.getText().toString().trim();
+
+        progressDialog.setMessage("Registering Consumer....");
+        progressDialog.show();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST,
+                Constants.URL_REGISTER,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        progressDialog.dismiss();
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+
+                            Toast.makeText(getApplicationContext(), jsonObject.getString("message"),Toast.LENGTH_LONG).show();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        progressDialog.hide();
+                        Toast.makeText(getApplicationContext(), error.getMessage(),Toast.LENGTH_SHORT).show();
+                    }
+                }
+        ){
+            @Override
+            protected Map<String, String> getPostParams() throws AuthFailureError {
+                Map<String,String> params= new HashMap<>();
+                params.put("lpg_id",lpg_id);
+                params.put("first_name",first_name);
+                params.put("last_name",last_name);
+                params.put("contact_no",contact_no);
+                params.put("email_id",email_id);
+                params.put("state",state);
+                params.put("city",city);
+                params.put("pincode",pincode);
+                params.put("landmark",landmark);
+                params.put("addressline1",addressline1);
+                params.put("addressline2",addressline2);
+                params.put("adhaar_no",adhaar_no);
+                params.put("gst_no",gst_no);
+                params.put("password",password);
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonRegister)
+            registerUser();
+
     }
 }
